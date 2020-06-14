57a239c056683de78524720e5dfa585d5450ec8b
beautytasara27 <beautytasara@gmail.com>
2020-06-15 01:04:29 +0700

    UI

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,46 +8,46 @@ export default class Footer extends Component {
 
             <section id="footer">
                 <div className="container" >
-                    <div class="row text-center text-xs-center text-sm-left text-md-left">
-                        <div class="col-xs-12 col-sm-4 col-md-4">
+                    <div className="row text-center text-xs-center text-sm-left text-md-left">
+                        <div className="col-xs-12 col-sm-4 col-md-4">
                             <h5>Quick links</h5>
-                            <ul class="list-unstyled quick-links">
-                                <li><a href="/home"><i class="fa fa-angle-double-right"></i>Home</a></li>
-                                <li><a href="/about"><i class="fa fa-angle-double-right"></i>About</a></li>
+                            <ul className="list-unstyled quick-links">
+                                <li><a href="/home"><i className="fa fa-angle-double-right"></i>Home</a></li>
+                                <li><a href="/about"><i className="fa fa-angle-double-right"></i>About</a></li>
                             </ul>
                         </div>
-                        <div class="col-xs-12 col-sm-4 col-md-4">
+                        <div className="col-xs-12 col-sm-4 col-md-4">
                         <h5>Quick links</h5>
-                            <ul class="list-unstyled quick-links">
-                                <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>Home</a></li>
-                                <li><a href="https://www.fiverr.com/share/qb8D02"><i class="fa fa-angle-double-right"></i>About</a></li>
+                            <ul className="list-unstyled quick-links">
+                                <li><a href="https://www.fiverr.com/share/qb8D02"><i className="fa fa-angle-double-right"></i>Home</a></li>
+                                <li><a href="https://www.fiverr.com/share/qb8D02"><i className="fa fa-angle-double-right"></i>About</a></li>
                             </ul>
                         </div>
-                        <div class="col-xs-12 col-sm-4 col-md-4">
+                        <div className="col-xs-12 col-sm-4 col-md-4">
                             <h5>Quick links</h5>
-                            <ul class="list-unstyled quick-links">
-                                <li><a href="/forum"><i class="fa fa-angle-double-right"></i>Forum</a></li>
-                                <li><a href="signup"><i class="fa fa-angle-double-right"></i>SignUp</a></li>
+                            <ul className="list-unstyled quick-links">
+                                <li><a href="/forum"><i className="fa fa-angle-double-right"></i>Forum</a></li>
+                                <li><a href="/signup"><i className="fa fa-angle-double-right"></i>SignUp</a></li>
                             </ul>
                         </div>
                     </div>
-                    <div className="navy container fluid">
-                    <div class="row ">
-                        <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
-                            <ul class="list-unstyled list-inline social text-center">
-                                <li class="list-inline-item"><a href="#"><i class="fa fa-facebook"></i></a></li>
-                                <li class="list-inline-item"><a href="#"><i class="fa fa-twitter"></i></a></li>
-                                <li class="list-inline-item"><a href="#"><i class="fa fa-instagram"></i></a></li>
-                                <li class="list-inline-item"><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                                <li class="list-inline-item"><a href="#" target="_blank"><i class="fa fa-envelope"></i></a></li>
+                    <div classNameName="navy container fluid">
+                    <div className="row ">
+                        <div className="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+                            <ul className="list-unstyled list-inline social text-center">
+                                <li className="list-inline-item"><a href="/home"><i className="fa fa-facebook"></i></a></li>
+                                <li className="list-inline-item"><a href="/home"><i className="fa fa-twitter"></i></a></li>
+                                <li className="list-inline-item"><a href="/home"><i className="fa fa-instagram"></i></a></li>
+                                <li className="list-inline-item"><a href="/home"><i className="fa fa-google-plus"></i></a></li>
+                                <li className="list-inline-item"><a href="/home" target="_blank"><i className="fa fa-envelope"></i></a></li>
                             </ul>
                         </div>
                         <hr />
                     </div>
-                    <div class="row">
-                        <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+                    <div className="row">
+                        <div className="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
                             <p><u><a href="https://www.mentalhealth.social">Mental HealthFirst</a></u> is a Non-Profit Organisation for people with mental health issues.</p>
-                            <p class="h6">© All right Reversed.<a class="text-green ml-2" href="https://www.sunlimetech.com" target="_blank">Mentalhealthfirst.social</a></p>
+                            <p className="h6">© All right Reversed.<a className="text-green ml-2" href="https://www.sunlimetech.com" target="_blank">Mentalhealthfirst.social</a></p>
                         </div>
                         <hr />
                     </div>
