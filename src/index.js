2e69857e60711b01977c281d54375440b62ecdfe
beautytasara27 <beautytasara@gmail.com>
2020-06-15 02:09:46 +0700

    tests

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import dotenv from 'dotenv'
 
-dotenv.config()
 
 ReactDOM.render(
   <React.StrictMode>
