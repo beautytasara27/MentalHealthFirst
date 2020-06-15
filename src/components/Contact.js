6e7fe5f879c3f9b425e909c001a44a5cd0525b8e
beautytasara27 <beautytasara@gmail.com>
2020-06-15 12:37:37 +0700

    contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -17,14 +17,14 @@ export default class Contact extends Component {
                
                 <div className="container" style={{ backgroundColor: '#91BB7F', position: "center", zIndex: 1, padding: '60px' }}>
                     <h2 style={{ padding: '20px' }} className="text-center display-4 texty">Contact Us</h2>
-                    <form method="POST" data-netlify="true" id="contact-form">
+                    <form method="POST" data-netlify="true" id="contact-form" name="contact">
                         <div className="  form-group border-bottom  border-dark">
                             <label htmlFor="name">Name</label>
                             <input type="text" id="name" name="name" className="form-control" style={{ background: 'none', border: "none" }} />
                         </div>
                         <div className=" form-group border-bottom border-dark">
                             <label htmlFor="message">Message</label>
-                            <textarea name="message" id="message" style={{ background: 'none', border: "none" }} className="form-control" rows="1"></textarea>
+                            <input type="text" name="message" id="message" style={{ background: 'none', border: "none" }} className="form-control" rows="2"></input>
                         </div>
                         <button type="submit" className="btn btn-green-moon">Submit</button>
                     </form>
