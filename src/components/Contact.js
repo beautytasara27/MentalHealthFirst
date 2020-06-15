d523c6b2caaaf4c6cf84dbf316b1bcfc058437c0
beautytasara27 <beautytasara@gmail.com>
2020-06-15 14:01:41 +0700

    contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,16 +8,15 @@ export default class Contact extends Component {
     render() {
         //col-lg-6 col-md-6  col-sm-12 col-xs-12 col-xl-6 "
         return (
-
             <Jumbotron className="row justify-content-center" style={{ backgroundColor: "white", position: "center", paddingBottom: '200px', paddingTop: "10px" }}>
                 <div className=" col-lg-8 col-md-12  col-sm-12 col-xs-12 col-xl-6 " >
                     <p className=" text-center text" style={{ padding: '30px' }}  >You can use this form to contact us about anything or to provide feedback and suggestions.
 
-                    If you are looking for support, please post on our our forum instead!</p>
+            If you are looking for support, please post on our our forum instead!</p>
 
                     <div className="container" style={{ backgroundColor: '#91BB7F', position: "center", zIndex: 1, padding: '60px' }}>
                         <h2 style={{ padding: '20px' }} className="text-center display-4 texty">Contact Us</h2>
-                        <form method="post" name="contact">
+                        <form name="contact" method="post" >
                             <input type="hidden" name="form-name" value="contact" />
                             <div className="  form-group border-bottom  border-dark">
                                 <label htmlFor="name">Name</label>
@@ -31,7 +30,7 @@ export default class Contact extends Component {
                         </form>
                     </div>
                 </div>
-            </Jumbotron >
+            </Jumbotron>
 
         )
     }
